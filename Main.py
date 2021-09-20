b0b669a734317e302d3011e6b850fe5991adf2b2
Matheus Filipe Rodrigues <matheus.filipes.rodrigues@gmail.com>
2021-09-20 16:05:11 -0300

    Update Main.py
    Tratamento de erros e aperfeiçoamento de código.

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 # Função Novamente?
 def novamente():
     print("Deseja adicionar uma nova lista?")
-    resposta = input ("Digite S para SIM ou N para NÃO: ")
-    if resposta == 'S':
+    try:
+        resposta = input ("Digite S para SIM ou N para NÃO: ")[0].upper()
         print()
-        principal()
-    elif resposta == 'N':
-        print()
-        print("Fim.")
-    else:
-        print()
+        if resposta == 'S':
+            principal()
+        elif resposta == 'N':
+            print("Fim.")
+        else:
+            novamente()
+    except IndexError:  # Tratamento de erro
         novamente()
 
 # Função Principal
@@ -18,45 +19,48 @@ def principal():
 # Declaração das variáveis
     lista_de_compras = []
     contador = ["Primeiro", "Segundo", "Terceiro", "Quarto", "Quinto"]
-    i = 0
 
 # Usuário informa a lista
     print("Informe os produtos da lista de compras")
-    while i < 5:
-        print(contador[i],"item: ")
+    for i in range(5):
+        print(f'{contador[i]} item: ')   
         descricao = input("Descrição: ")
-        preco = float(input("Preço: "))
+        continuar = False
+        while not continuar:  # Tratamento de erro
+            try:
+                preco = float(input("Preço: "))
+            except ValueError:
+                print('Valor inválido! Tente novamente.')
+            else:
+                continuar = True
         print()
         item = {}
         item["Descrição"] = descricao
         item["Preço"] = preco
         lista_de_compras.append(item)
-        i+=1
 
 # Impressão da lista
-    i = 0
     print("Lista de Compras: ")
     print()
-    while i < 5:
-        print(lista_de_compras[i]["Descrição"],"........R$","%.2f" % lista_de_compras[i]["Preço"])
-        i+=1
+    for i in range(5):
+        print(f'{lista_de_compras[i]["Descrição"]} .......... R${lista_de_compras[i]["Preço"]:.2f}')  # Aperfeiçoamento de código
 
 # Mais caro e mais barato
-    i = 0
     produto_caro = {}
     produto_barato = {}
-    while i < 5:
-        if i == 0:
-            produto_caro = lista_de_compras[i]
-            produto_barato = lista_de_compras[i]
-        else:
-            if float(lista_de_compras[i]["Preço"]) > produto_caro["Preço"]:
-                produto_caro = lista_de_compras[i]
-            elif float(lista_de_compras[i]["Preço"]) < produto_barato["Preço"]:
-                produto_barato = lista_de_compras[i]
-        i+=1
-    print("O produto mais caro é",produto_caro["Descrição"],"por","%.2f" % produto_caro["Preço"])
-    print("O produto mais barato é",produto_barato["Descrição"],"por","%.2f" % produto_barato["Preço"])
+    precos = list()
+    descs = list()
+
+    for item in lista_de_compras:
+        descs.append(item['Descrição'])
+        precos.append(item['Preço'])
+    produto_caro["Descrição"] = descs[precos.index(max(precos))]
+    produto_caro["Preço"] = max(precos)
+    produto_barato["Descrição"] = descs[precos.index(min(precos))]
+    produto_barato["Preço"] = min(precos)
+
+    print(f'O produto mais caro é {produto_caro["Descrição"].upper()} por R${produto_caro["Preço"]:.2f}')
+    print(f'O produto mais barato é {produto_barato["Descrição"].upper()} por R${produto_barato["Preço"]:.2f}')
     print()
 
 # EXECUÇÃO
